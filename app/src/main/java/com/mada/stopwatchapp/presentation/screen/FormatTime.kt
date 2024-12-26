fe2add7a801156dd41a98f5f4350ad47061ed50b
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2024-12-26 13:32:42 +0200

    Edit Screen design

--- app/src/main/java/com/mada/stopwatchapp/presentation/screen/FormatTime.kt
+++ app/src/main/java/com/mada/stopwatchapp/presentation/screen/FormatTime.kt
@@ -5,5 +5,5 @@ fun FormatTime(milliseconds: Long): String {
     val seconds = (milliseconds % 60000) / 1000
     val millis = (milliseconds % 1000) / 10
 
-    return "%02d:%02d:%02d".format(minutes, seconds, millis)
+    return "%02d : %02d : %02d".format(minutes, seconds, millis)
 }
--- app/src/main/java/com/mada/stopwatchapp/presentation/screen/StopwatchScreen.kt
+++ app/src/main/java/com/mada/stopwatchapp/presentation/screen/StopwatchScreen.kt
@@ -1,21 +1,28 @@
 package com.mada.stopwatchapp.presentation.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mada.stopwatchapp.presentation.viewmodel.StopwatchViewModel
 
@@ -42,30 +49,51 @@ fun StopwatchContent(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color(0xFF121212))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = FormatTime(time),
-            style = MaterialTheme.typography.headlineLarge
-        )
+        Box(
+            modifier = Modifier
+                .background(Color(0xFF1F1F1F), shape = RoundedCornerShape(16.dp))
+                .padding(24.dp)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        ) {
+            Text(
+                text = FormatTime(time),
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
 
         Row(
             horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Button(onClick = onStart) {
-                Text("Start")
+            Button(
+                onClick = onStart,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF388E3C)),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Start", color = Color.White)
             }
 
-            Button(onClick = onPause) {
-                Text("Pause")
+            Button(
+                onClick = onPause,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF388E3C)),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Pause", color = Color.White)
             }
 
-            Button(onClick = onReset) {
-                Text("Reset")
+            Button(
+                onClick = onReset,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF388E3C)),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("Reset", color = Color.White)
             }
         }
     }
